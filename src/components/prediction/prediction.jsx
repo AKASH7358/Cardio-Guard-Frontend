3e8b683d5817f9c27bd2b034d14d9fb0dfd98d17
AKASH7358 <121285908+AKASH7358@users.noreply.github.com>
2025-04-08 22:30:26 +0530

    Update prediction.jsx

--- src/components/prediction/prediction.jsx
+++ src/components/prediction/prediction.jsx
@@ -66,7 +66,7 @@ const Prediction = () => {
         formData.append("file", selectedFile, "image.png");
 
         try {
-            const response = await fetch("http://127.0.0.1:10000/predict", {
+            const response = await fetch("https://AKASH7358-heart-disease-api.hf.space/predict", {
                 method: "POST",
                 body: formData,
             });
